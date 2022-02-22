d86c9cb3be742d28ae6524d9c99ecba8e994d932
phuonghq <halohalo831999@gmail.com>
2022-02-22 17:11:35 +0700

    add slick slide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'danhmucsuckhoe-app';
+  slides = [
+    { img: '../assets/img/positive-girl-dentist-1.png' },
+    { img: '../assets/img/positive-girl-dentist-2.png' },
+    { img: '../assets/img/positive-girl-dentist-3.png' },
+    { img: '../assets/img/positive-girl-dentist-4.png' },
+    { img: '../assets/img/positive-girl-dentist-5.png' },
+    { img: '../assets/img/positive-girl-dentist-1.png' },
+    { img: '../assets/img/positive-girl-dentist-2.png' },
+    { img: '../assets/img/positive-girl-dentist-3.png' },
+    { img: '../assets/img/positive-girl-dentist-4.png' },
+    { img: '../assets/img/positive-girl-dentist-5.png' },
+  ];
+  slideConfig = { slidesToShow: 4, slidesToScroll: 4 };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
